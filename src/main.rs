876ad118adf6e747d8af42f966f338f6501f8742
Anurag <anurg@yahoo.com>
2024-10-07 23:03:04 +0530

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,9 @@
 fn main() {
-    //     let _sentence: String = String::from("Hello World!, Here I come");
-    //     let first_word = get_first_word(_sentence);
-    //     let n = 1000;
-    //     for i in 0..n {
-    //         println!("Loop No: {}", i);
-    //     }
-    //     println!("The first word is : {} ", first_word);
-    // }
-
-    // pub fn get_first_word(my_str: String) -> String {
-    //     let mut ans: String = String::from("");
-    //     for char in my_str.chars() {
-    //         // ans.push(char);
-    //         ans.push_str(char.to_string().as_str());
-    //         if char == ' ' {
-    //             break;
-    //         }
-    //     }
-    //     return ans;
-    let a = 5;
-    let b = 10;
-    println!("The sum of {} and {} is {}", a, b, do_sum(a, b));
+    let str:String = String::from("Hello New AI World!");
+    let n = get_string_length(str);
+    println!("The length of string is {}",n);
 }
 
-pub fn do_sum(a: i32, b: i32) -> i32 {
-    return a + b;
+fn get_string_length(str:String) -> usize {
+    return str.chars().count();
 }
