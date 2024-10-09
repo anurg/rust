e5c4f489e82bde4627bcba9ea16d9c1a10ff5bc1
Anurag <anurg@yahoo.com>
2024-10-09 16:03:48 +0530

    Options Enum

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,17 @@
-use std::f64::consts::PI;
-
-enum Shape {
-    Circle(f64),
-    Square(f64),
-    Rectangle(f64,f64),
-    
+fn find_first_a(s: String) -> Option<i32> {
+    for (index, character) in s.chars().enumerate() {
+        if character == 'a' {
+            return Some(index as i32);
+        }
+    }
+    return None;
 }
 
-fn calculate_area(shape:Shape)-> f64 {
-    let area = match shape {
-        Shape::Circle(radius)=> PI * radius * radius,
-        Shape::Square(side) => side * side,
-        Shape::Rectangle(length,width)=> length * width,
-        };
-        return area;
-}
 fn main() {
-    let circle = Shape::Circle(9.1);
-    let square = Shape::Square(4.4);
-    let rect = Shape::Rectangle(6.0, 8.0);
-
-    println!("The are of the Circle is {}", calculate_area(circle) );
-    println!("The are of the Square is {}", calculate_area(square) );
-    println!("The are of the Rectangle is {}", calculate_area(rect) );
+    let my_string = String::from("Testing Rust");
+    let index = find_first_a(my_string);
+    match index {
+        Some(value) => println!("The index is {}", value),
+        None => println!("a not found!"),
+    }
 }
-
