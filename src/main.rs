b7c1d78b7a8510e70dc1ef78f52f50aac4207fcc
Anurag <anurg@yahoo.com>
2024-10-09 16:16:02 +0530

    Added Results enum

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
-fn find_first_a(s: String) -> Option<i32> {
-    for (index, character) in s.chars().enumerate() {
-        if character == 'a' {
-            return Some(index as i32);
-        }
-    }
-    return None;
-}
+use std::fs;
 
 fn main() {
-    let my_string = String::from("Testing Rust");
-    let index = find_first_a(my_string);
-    match index {
-        Some(value) => println!("The index is {}", value),
-        None => println!("a not found!"),
+    let contents_of_file = fs::read_to_string("hello.txt");
+    match contents_of_file {
+        Ok(file_content) => {
+            println!("The contents of file {:?} ", file_content)
+        }
+        Err(error) => {
+            println!(" Failed to Read File! {:?}", error)
+        }
     }
 }
