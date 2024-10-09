941b56f06dc7c329488976a5fc593eac4469f5c8
Anurag <anurg@yahoo.com>
2024-10-09 17:46:35 +0530

    commit

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::fs;
 
 fn main() {
     let contents_of_file = fs::read_to_string("hello.txt");
-    match contents_of_file {
+    let ans = match contents_of_file {
         Ok(file_content) => {
             println!("The contents of file {:?} ", file_content)
         }
         Err(error) => {
             println!(" Failed to Read File! {:?}", error)
         }
-    }
+    };
+    return ans;
 }
