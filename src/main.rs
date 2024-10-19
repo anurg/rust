3066a9c0aeb2f612a744a93f1c0b7c1147df2afe
Anurag <anurg@yahoo.com>
2024-10-19 17:40:54 +0530

    ...

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use std::fs;
-
 fn main() {
-    let contents_of_file = fs::read_to_string("hello.txt");
-    let ans = match contents_of_file {
-        Ok(file_content) => {
-            println!("The contents of file {:?} ", file_content)
-        }
-        Err(error) => {
-            println!(" Failed to Read File! {:?}", error)
-        }
-    };
-    return ans;
+    let num: i32 = 41;
+    for i in 0..num {
+        println!("The number: {} ", i);
+    }
+    println!("Hello Rustacean {}", "Anurag");
+    println!("The result: {} {}", num, is_even(num));
+}
+
+fn is_even(num: i32) -> String {
+    if num % 2 == 0 {
+        return String::from("is Even");
+    } else {
+        return String::from("is not Even");
+    }
+}
+
+fn get_str_len(str: String) -> usize {
+    return str.chars().count();
 }
