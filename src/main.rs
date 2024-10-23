e757536a896b0f19a523d474705a6a7e7b8ec772
Anurag <anurg@yahoo.com>
2024-10-23 23:55:16 +0530

    while let.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 
 fn main() {
-    let mut nums= vec![1,2,3,4,5];
-    let iter = nums.iter_mut();
-    for val in iter {
-        *val = *val * 2;
+    let nums= vec![1,2,3,4,5];
+        let mut nums_iter = nums.iter();
+        while let Some(val) = nums_iter.next() {
+            println!("{}",val);
+        }
+        println!("{:?}",nums);
     }
-    println!("{:?}",nums);
-}
+    
