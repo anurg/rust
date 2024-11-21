aa9c0199e4ad952cc8906b134ef30e6040aee81b
Anurag <anurg@yahoo.com>
2024-11-21 16:27:39 +0530

    Added solana smart contract

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-
+pub fn main() {
+    let hexadecimal = 0x10;
+    let octal = 0o10;
+    let binary = 0b10;
+    let mut n = 10;
+    println!("{}", n);
+    n = hexadecimal;
+    println!("{}", n);
+    n = octal;
+    println!("{}", n);
+    n = binary;
+    println!("{}", n);
+}
